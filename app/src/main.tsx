0373f6a0db3ec842c2a10aa6c7e6af75a4b66ea3
rafaeelricco <rafaelricco10@gmail.com>
2026-09-19 20:08:18 -0300

    Use path alias for CSS import and move it to top

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,7 @@
+import "@/index.css"
+
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-
-import "./index.css"
 import { ThemeProvider } from "@components/ui/theme-provider"
 import { Toaster } from "@ui/sonner"
 import { Router } from "@/router"
